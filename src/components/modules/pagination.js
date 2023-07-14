9179e84c9591c81b2e338fb7e4d014529a8febc6
Luka <lukasouza77@gmail.com>
2023-07-14 06:13:44 -0300

    '.'

--- src/components/modules/pagination.js
+++ src/components/modules/pagination.js
@@ -1,12 +1,34 @@
-
+import { useState } from "react";
 
 export default function Pagination({ children }) {
-
-
-
+    const [currentPage, setCurrentPage] = useState(1);
+  
+    const handlePageChange = (pageNumber) => {
+      setCurrentPage(pageNumber);
+    };
+    if  (window.innerWidth > "550") {
+        var itemsPerPage = 5;
+    } else if (window.innerWidth < "550") {
+        // eslint-disable-next-line
+        var itemsPerPage = 10;
+    }
+  
     return (
-        <div className="pagination">
-            {children}
+      <>
+        {children.slice(
+          (currentPage - 1) * itemsPerPage,
+          currentPage * itemsPerPage
+        )}
+        <div className="pagination-controls">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          ><i className="fa-solid fa-angle-left" /></button>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === Math.ceil(children.length / itemsPerPage)}
+          ><i className="fa-solid fa-angle-right" /></button>
         </div>
-    )
+      </>
+    );
 }
